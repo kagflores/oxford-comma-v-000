1e8e6af594b99ce73b876b45fb97e8371fa80193
Karen Flores <karen.ag.flores@gmail.com>
2018-03-30 08:09:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,10 @@ def oxford_comma(array)
   elsif array.size == 2
     and_join = array.join (" and ")
     return and_join
-  else array.size == 3
-    comma_and_join = array.join (",", "and")
+  elsif array.size == 3
+    comma_and_join = array.join (",")
     return comma_and_join
+  else 
+    all_other_join = array.join (",")
   end
 end
