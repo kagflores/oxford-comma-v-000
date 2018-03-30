b8f68167e18080244ff652b156b7c75d8d1eff94
Karen Flores <karen.ag.flores@gmail.com>
2018-03-30 08:24:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,10 +5,11 @@ def oxford_comma(array)
   elsif array.size == 2
     and_join = array.join (" and ")
     return and_join
-  elsif array.size == 3
-    comma_and_join = array.join (",")
-    return comma_and_join
   else
-    all_other_join = array.join (" , ")
+    last_element = array.pop
+    comma_join = array.join (",")
+    last_element = " and #{last_element}"
+    comma_and_join = comma_join << last_element
+    return comma_and_join
   end
 end
